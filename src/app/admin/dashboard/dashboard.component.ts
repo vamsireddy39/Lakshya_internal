a5a37a633d95e6925e1c53af1341fa01e287bdb8
Yamini1137 <yamini.srikakulapu@gmail.com>
2024-09-05 20:53:36 +0530

    login changes

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../../shared.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  userLength: number | undefined; // Correct type annotation
+  constructor(public sharedService: SharedService) { }
+  ngOnInit() {
+    const sessionKey = this.sharedService.getSessionKey(); // Retrieve session key
+
+    if (sessionKey) {
+      console.log('Session Key:', sessionKey); // Debug: Print session key to verify
+
+      const headers = new HttpHeaders().set('X-Session-Key', ` ${sessionKey}`);
+      this.sharedService.getUserData().subscribe((response: any) => {
+        console.log(response); // Handle the response inside the subscribe callback
+        this.userLength = response.length; // Assign the response length
+      });
+    } else {
+      console.error('Session key is missing.');
+    }
+  }
 
 }
